4cf9b82e2dcb5146689fc1b3fe542409eabdd27b
chambers325 <mike.chambers@VIBES-MCHAMBERS-3.local>
2013-03-15 08:13:12 -0500

    Created nothing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrails32app::Application.config.secret_token = '74e2eea3d1943819a489e09365c92b5c3dd5d42a597503d3850c97552c6d3dd7ba097b9cbab922aa339c798d5b5e0064bc25aaac1e79552f1ae809d618259de7'
